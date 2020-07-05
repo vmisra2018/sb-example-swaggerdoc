a89316d17797e971a09f0d2816023e0a3f74cc8c
Vivek Misra <vivekmisrajobs@gmail.com>
2020-07-05 15:04:57 -0700

    first commit

--- src/main/java/org/vivek/springboot/example/swaggerdoc/PersonController.java
+++ src/main/java/org/vivek/springboot/example/swaggerdoc/PersonController.java
@@ -1,4 +1,6 @@
 package org.vivek.springboot.example.swaggerdoc;
+import java.net.http.HttpRequest;
+
 import javax.validation.Valid;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonController {
     @RequestMapping(path = "/person", method = RequestMethod.POST)
     public Person person(@Valid @RequestBody Person person) {
+        return person;
+    }
+    
+    @RequestMapping(path = "/personrequest", method = RequestMethod.POST)
+    public Person personHttpRequest(HttpRequest req) {
+    	Person person = new Person();
         return person;
     }
 }
