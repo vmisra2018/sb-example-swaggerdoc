61040d681bc3aa3f3eae2949f674f930f0ca92fa
Vivek Misra <vivekmisrajobs@gmail.com>
2020-07-30 13:29:25 -0700

    added health metrics

---
+++ src/main/java/org/vivek/springboot/example/swaggerdoc/api/rest/ServiceAHealthIndicator.java
@@ -0,0 +1,25 @@
+package org.vivek.springboot.example.swaggerdoc.api.rest;
+
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServiceAHealthIndicator implements HealthIndicator {
+        private final String message_key = "Service A";
+    @Override
+    public Health health() {
+        if (!isRunningServiceA()) {
+            return Health.down().withDetail(message_key, "Not Available").build();
+        }
+        return Health.up().withDetail(message_key, "Available").build();
+    }
+
+    private Boolean isRunningServiceA() {
+        Boolean isRunning = true;
+        // Logic Skipped
+
+        return isRunning;
+    }
+}
